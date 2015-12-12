ed8f313bf120bba6a4cea1cf4c5a3ab98b5fdf5d
Mark Stemm <mstemm@jut.io>
2015-12-11 18:16:55 -0800

    Clean up auth steps.
    Fill in the auth section with the required steps to create client
    credentials and a user oauth2 token.
    
    Add a helper program create_oauth_token.js that creates the oauth2
    token and provides a suggested block to add to
    .juttle/config.{js,json}.
    
    Update the backend to read both config elements from
    .juttle/config.{js,json} instead of from the files on disk.

--- README.md
+++ README.md
@@ -21,11 +21,100 @@ Make sure the following is in your environment:
 
 # Configuration
 
+Configuration involves these steps:
+1. Create application credentials that allow your code to access the google nodejs APIs.
+2. Authorize a user using Oauth2 to use the application to access gmail.
+3. Add the appropriate configuration items to `.juttle/config.{js,json}`
+
+## Create application credentials
+
+To create application credentials, follow the
+[nodejs quickstart instructions](https://developers.google.com/gmail/api/quickstart/nodejs). This
+will result in a file on disk titled `client_secret.json` with this structure:
+
+```
+{
+  "installed": {
+    "client_id": "--your-client-id--",
+    "project_id": "--your-project-id",
+    "auth_uri": "https:\/\/accounts.google.com\/o\/oauth2\/auth",
+    "token_uri": "https:\/\/accounts.google.com\/o\/oauth2\/token",
+    "auth_provider_x509_cert_url": "https:\/\/www.googleapis.com\/oauth2\/v1\/certs",
+    "client_secret": "--your-client-secret-id--",
+    "redirect_uris": [
+      "urn:ietf:wg:oauth:2.0:oob",
+      "http:\/\/localhost"
+    ]
+  }
+}
+```
+
+You'll use this file in the next step.
+
+## Authorize a user using OAuth2
+
 You need to create an oauth2 token that allows this program to read your email on your behalf.
 
-XXX/mstemm this process is very cumbersome, I'm looking into easier
-ways to get it configured and set up and will update this section once
-it's streamlined.
+To do this, run `node create_oauth_token.js` from the gmail-backend directory.
+
+This will provide a json config block to add to your `.juttle/config.{js,json}` file.
+
+This will also use the gmail nodejs api to read the list of labels
+assocated with the authenticated user, to verify that the token was created successfully.
+
+## Add the appropriate configuration items to `.juttle/config.{js,json}`
+
+`create_oauth_token.js` printed a configuration block like this:
+
+```
+{
+  "backends": {
+    "juttle-gmail-backend": {
+      "client-credentials": {
+        "installed": {
+          "client_id": "--your-client-id--",
+          "project_id": "--your-project-id",
+          "auth_uri": "https:\/\/accounts.google.com\/o\/oauth2\/auth",
+          "token_uri": "https:\/\/accounts.google.com\/o\/oauth2\/token",
+          "auth_provider_x509_cert_url": "https:\/\/www.googleapis.com\/oauth2\/v1\/certs",
+          "client_secret": "--your-client-secret-id--",
+          "redirect_uris": [
+            "urn:ietf:wg:oauth:2.0:oob",
+            "http:\/\/localhost"
+          ]
+        }
+      },
+      "oauth2-token": {
+        "access_token": "---your-access-token---",
+        "token_type": "Bearer",
+        "refresh_token": "---your-refresh-token---",
+        "expiry_date": DDDDDDDDDDDDD
+      }
+    }
+  }
+}
+```
+
+Add this configuration to your `.juttle/config.{js,json}` file. If you
+have an existing "backends" section, for example:
+
+```
+{
+  "backends": {
+    "juttle-twitter-backend": {...}
+  }
+}
+```
+
+Add the juttle-gmail-backend section as a peer item below "backends":
+```
+{
+  "backends": {
+    "juttle-twitter-backend": {...},
+    "juttle-gmail-backend": {...}
+  }
+}
+```
 
 # Usage
 
---
+++ create_oauth_token.js
@@ -0,0 +1,131 @@
+// Adapted from quickstart.js at https://developers.google.com/gmail/api/quickstart/nodejs
+
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+var TOKEN_PATH = 'gmail-nodejs-quickstart.json';
+
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+    if (err) {
+        console.log('Error loading client secret file: ' + err);
+        return;
+    }
+    // Authorize a client with the loaded credentials, then call the
+    // Gmail API to test that the process was successful
+    authorize(JSON.parse(content), listLabels);
+});
+
+/**
+  * Create an OAuth2 client with the given credentials, and then execute the
+  * given callback function.
+  *
+  * @param {Object} credentials The authorization client credentials.
+  * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+    var clientSecret = credentials.installed.client_secret;
+    var clientId = credentials.installed.client_id;
+    var redirectUrl = credentials.installed.redirect_uris[0];
+    var auth = new googleAuth();
+    var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function(err, token) {
+        if (err) {
+            getNewToken(credentials, oauth2Client, callback);
+        } else {
+            oauth2Client.credentials = JSON.parse(token);
+            callback(oauth2Client);
+        }
+    });
+}
+
+/**
+  * Get and store new token after prompting for user authorization, and then
+  * execute the given callback with the authorized OAuth2 client.
+  *
+  * @param {Object} credentials The authorization client credentials.
+  * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+  * @param {getEventsCallback} callback The callback to call with the authorized
+  *     client.
+ */
+function getNewToken(credentials, oauth2Client, callback) {
+    var authUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES
+    });
+    console.log('Authorize this app by visiting this url: ', authUrl);
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Enter the code from that page here: ', function(code) {
+        rl.close();
+        oauth2Client.getToken(code, function(err, token) {
+            if (err) {
+                console.log('Error while trying to retrieve access token', err);
+                return;
+            }
+            oauth2Client.credentials = token;
+            storeToken(credentials, token);
+            callback(oauth2Client);
+        });
+    });
+}
+
+/**
+  * Store token to disk be used in later program executions.
+  *
+  * @param {Object} credentials The authorization client credentials.
+  * @param {Object} token The token to store to disk.
+ */
+function storeToken(credentials, token) {
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+    console.log('Token stored to ' + TOKEN_PATH);
+
+    console.log('Add the following json block to your .juttle/config.{js,json} file:');
+    var juttle_config = {
+        backends: {
+            "juttle-gmail-backend": {
+                "client-credentials": credentials,
+                "oauth2-token": token,
+            }
+        }
+    };
+
+    console.log(JSON.stringify(juttle_config, null, 4));
+
+    console.log("If you already have a \"backends\" section, add the \"juttle-gmail-backend\" section under the existing \"backends\" section.");
+}
+
+/**
+  * Lists the labels in the user's account.
+  *
+  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listLabels(auth) {
+    var gmail = google.gmail('v1');
+    gmail.users.labels.list({
+        auth: auth,
+        userId: 'me',
+    }, function(err, response) {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+        var labels = response.labels;
+        if (labels.length == 0) {
+            console.log('No labels found.');
+        } else {
+            console.log('Labels:');
+            for (var i = 0; i < labels.length; i++) {
+                var label = labels[i];
+                console.log('- %s', label.name);
+            }
+        }
+    });
+}
